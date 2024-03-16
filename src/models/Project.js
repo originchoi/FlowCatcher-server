5f41aba677b10c873476be0d3ad0142dac9cfeb2
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-03-17 03:22:30 +0900

    Feat: project schema

---
+++ src/models/Project.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  projectName: {
+    type: String,
+    required: true,
+  },
+  apiKey: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Project", projectSchema);
