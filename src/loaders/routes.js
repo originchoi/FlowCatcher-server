0f5a1ead4d0470250438addbac35e5a9a4083310
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-03-20 03:44:18 +0900

    Feat: session router settings for business users

--- src/loaders/routes.js
+++ src/loaders/routes.js
@@ -1,11 +1,13 @@
 const index = require("../routes/index");
 const authRouter = require("../routes/auth");
 const projecstRouter = require("../routes/projects");
+const sessionRouter = require("../routes/sessions");
 
 function connectRouters(app) {
   app.use("/", index);
   app.use("/auth", authRouter);
   app.use("/users", projecstRouter);
+  app.use("/api", sessionRouter);
 }
 
 module.exports = connectRouters;
---
+++ src/routes/sessions.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+const verifyApiKey = require("../middlewares/verifyApiKey");
+const Session = require("../models/Session");
+
+router.post("/sessions", verifyApiKey, async function (req, res, next) {
+  try {
+    const { projectId } = req.body;
+    const newSession = new Session({ projectId });
+    const savedSession = await newSession.save();
+
+    res.status(201).json({ sessionId: savedSession._id });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
